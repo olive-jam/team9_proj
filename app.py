dc073469913bf68550b98a58d3cd7502b523c4c0
olive-jam <120158932+olive-jam@users.noreply.github.com>
2022-12-11 04:05:19 +0900

    댓글역순

--- app.py
+++ app.py
@@ -38,31 +38,22 @@ def review_post():
 def review_done():
     num_receive = request.form['num_give']
     db.review.update_one({'num':int(num_receive)},{'$set':{'done':1}})
-    return jsonify({'msg': '✏️ 수정완료'})
-
-
-
-
-
+    return jsonify({'msg': '✏️ 등록완료'})
 
 
 
 @app.route("/review/cancel", methods=["POST"])
-def review_cancel():
+def cancel_review():
     num_receive = request.form['num_give']
-    db.review.update_one({'num': int(num_receive)},{'$set':{'done':0}})
+    db.review.delete_one({'num':int(num_receive)})
     return jsonify({'msg': '☠️ 삭제완료'})
 
-# @app.route("/review/cancel", methods=["POST"])
-# def review_cancel():
-#     num_receive = request.form['num_give']
-#     db.review.update_one({'num': int(num_receive)},{'$set':{'done':0}})
-#     return jsonify({'msg': ''})
 
 
 @app.route("/review", methods=["GET"])
 def review_get():
     review_list = list(db.review.find({},{'_id':False}))
+
 
     return jsonify({'reviews': review_list})
 
